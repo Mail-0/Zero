6f4f50707559f5c31ae41f79d3c7d661566eb6b4
Prashant Varma <prashantvarma5083@gmail.com>
2025-02-22 22:39:01 +0530

    feat: added a early access card

---
+++ app/(full-width)/early-access/page.tsx
@@ -0,0 +1,12 @@
+import EaryAccessPage from "@/components/ui/early-access-pass";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex h-screen w-[100%] items-center justify-center">
+      <EaryAccessPage />
+    </div>
+  );
+};
+
+export default page;
