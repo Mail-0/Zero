effdd58f7241cf2b404fe8c45f0847a6f7bbd57a
nafees nazik <nafeesnazik21@gmail.com>
2025-05-07 10:48:31 +0530

    fix: migration

--- packages/db/migrations/0026_conscious_bug.sql
+++ packages/db/migrations/0026_conscious_bug.sql
@@ -15,7 +15,6 @@ CREATE TABLE "mail0_theme" (
 	"updated_at" timestamp NOT NULL
 );
 --> statement-breakpoint
-ALTER TABLE "mail0_user_settings" ALTER COLUMN "settings" SET DEFAULT '{"language":"en","timezone":"UTC","dynamicContent":false,"externalImages":true,"customPrompt":"","trustedSenders":[],"isOnboarded":false,"colorTheme":"system"}'::jsonb;--> statement-breakpoint
 ALTER TABLE "mail0_connection_theme" ADD CONSTRAINT "mail0_connection_theme_connection_id_mail0_connection_id_fk" FOREIGN KEY ("connection_id") REFERENCES "public"."mail0_connection"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "mail0_connection_theme" ADD CONSTRAINT "mail0_connection_theme_theme_id_mail0_theme_id_fk" FOREIGN KEY ("theme_id") REFERENCES "public"."mail0_theme"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "mail0_theme" ADD CONSTRAINT "mail0_theme_user_id_mail0_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."mail0_user"("id") ON DELETE cascade ON UPDATE no action;
