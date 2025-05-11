eee130c0aaa38cf4bb16d7102d5b217b845ff111
f0rc <80628768+f0rc@users.noreply.github.com>
2025-05-11 14:20:03 -0500

    feat: added ability to select email text

--- apps/mail/components/ui/nav-user.tsx
+++ apps/mail/components/ui/nav-user.tsx
@@ -176,7 +176,7 @@ export function NavUser() {
                         src={activeAccount?.picture || undefined}
                         alt={activeAccount?.name || activeAccount?.email}
                       />
-                      
+
                       <AvatarFallback className="rounded-[5px] text-[10px]">
                         {(activeAccount?.name || activeAccount?.email)
                           .split(' ')
@@ -563,7 +563,7 @@ export function NavUser() {
                 <BadgeCheck className="h-4 w-4 text-white dark:text-[#141414]" fill="#1D9BF0" />
               )}
             </div>
-            <div className="max-w-[150px] overflow-hidden truncate text-xs font-normal leading-none text-[#898989]">
+            <div className="max-w-[150px] select-text overflow-hidden truncate text-xs font-normal leading-none text-[#898989]">
               {activeAccount?.email || session.user.email}
             </div>
           </div>
