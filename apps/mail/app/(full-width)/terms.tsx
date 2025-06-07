97891b7ba4aff9401c2dff7ed96a38f3a6b1a0e4
Valentin Chmara <valentinchmara@gmail.com>
2025-06-07 22:13:33 +0200

    refactor: avoid conflict medium breakpoint

--- apps/mail/app/(full-width)/terms.tsx
+++ apps/mail/app/(full-width)/terms.tsx
@@ -25,7 +25,7 @@ export default function TermsOfService() {
       <Navigation />
       <div className="relative z-10 flex flex-grow flex-col">
         {/* Back Button */}
-        <div className="absolute md:left-8 md:top-8 right-4 top-6">
+        <div className="absolute right-4 top-6 md:left-8 md:top-8 md:right-auto">
           <a href="/">
             <Button
               variant="ghost"
