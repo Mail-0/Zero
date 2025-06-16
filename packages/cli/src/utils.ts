ba11d3dce3c301f7f724cc7f3c8d37bb0d3b9201
Fahad <fahad.khan2216@gmail.com>
2025-06-17 03:28:49 +0530

    nizzy sync utils.ts updated for windows

--- packages/cli/src/utils.ts
+++ packages/cli/src/utils.ts
@@ -16,7 +16,16 @@ export const getProjectRoot = async () => {
 };
 
 export const runCommand = async (command: string, args: string[], options: SpawnOptions = {}) => {
-  const child = spawn(command, args, { stdio: 'inherit', ...options });
+  const isWindows = process.platform === 'win32';
+  const finalCommand = isWindows ? 'cmd.exe' : command;
+  const finalArgs = isWindows ? ['/c', command, ...args] : args;
+  
+  const child = spawn(finalCommand, finalArgs, { 
+    stdio: 'inherit', 
+    shell: true,
+    ...options 
+  });
+
   await new Promise((resolve, reject) => {
     child.once('close', resolve);
     child.once('error', reject);
