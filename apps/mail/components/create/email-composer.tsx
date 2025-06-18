39dcff95b50d88c349feed071976f6d048555ec7
Manik Sharma <maniksharma424@gmail.com>
2025-06-18 11:46:48 +0530

    fixed email composer bottom pannel breaking issue if email content grows past 500px height

--- apps/mail/components/create/email-composer.tsx
+++ apps/mail/components/create/email-composer.tsx
@@ -459,7 +459,7 @@ export function EmailComposer({
         className,
       )}
     >
-      <div className="no-scrollbar dark:bg-panelDark max-h-[500px] grow overflow-y-auto">
+      <div className="no-scrollbar dark:bg-panelDark  grow overflow-y-auto">
         {/* To, Cc, Bcc */}
         <div className="shrink-0 overflow-y-auto border-b border-[#E7E7E7] pb-2 dark:border-[#252525]">
           <div className="flex justify-between px-3 pt-3">
