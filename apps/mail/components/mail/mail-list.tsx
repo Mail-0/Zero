7f6233f520c554cf106332f8827a0b7f51c0122c
Fahad <fahad.khan2216@gmail.com>
2025-06-28 16:59:37 +0530

    changed noname placeholder to No Reciepient

--- apps/mail/components/mail/mail-list.tsx
+++ apps/mail/components/mail/mail-list.tsx
@@ -664,7 +664,7 @@ const Draft = memo(({ message }: { message: { id: string } }) => {
                     )}
                   >
                     <span className={cn('max-w-[25ch] truncate text-sm')}>
-                      {cleanNameDisplay(draft?.to?.[0] || 'noname') || ''}
+                      {cleanNameDisplay(draft?.to?.[0] || 'No Reciepient') || ''}
                     </span>
                   </span>
                 </div>
