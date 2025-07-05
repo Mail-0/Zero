a9221d0d6de5ae5b827e8d9a5b97c73d429f9d1e
Ahmet Kilinc <akx9@icloud.com>
2025-07-05 19:57:11 +0100

    fixes to email rendering

--- apps/server/src/lib/email-processor.ts
+++ apps/server/src/lib/email-processor.ts
@@ -14,7 +14,7 @@ export function processEmailHtml({ html, shouldLoadImages, theme }: ProcessEmail
   let hasBlockedImages = false;
 
   const sanitizeConfig: sanitizeHtml.IOptions = {
-    allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'title']),
     allowedAttributes: false,
     allowedSchemes: shouldLoadImages
       ? ['http', 'https', 'mailto', 'tel', 'data', 'cid', 'blob']
