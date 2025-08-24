5ca5c9bd77824c4b9ce8d5cef44e6ec32eafa560
Furquan Anwer <furquan.anwer@gmail.com>
2025-08-24 17:20:12 +0530

    fixed inconsistent vertical spacing in navigation links in mobile view by removing extra margin

--- apps/mail/components/navigation.tsx
+++ apps/mail/components/navigation.tsx
@@ -243,10 +243,10 @@ export function Navigation() {
             </SheetHeader>
             <div className="mt-8 flex flex-col space-y-3">
               <div className="flex flex-col space-y-3">
-                <Link to="/" className="mt-2" onClick={() => setOpen(false)}>
+                <Link to="/" onClick={() => setOpen(false)}>
                   Home
                 </Link>
-                <Link to="/pricing" className="mt-2" onClick={() => setOpen(false)}>
+                <Link to="/pricing" onClick={() => setOpen(false)}>
                   Pricing
                 </Link>
                 {aboutLinks.map((link) => (
